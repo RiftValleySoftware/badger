d83176c8792eecb2beb7781eabc2cf9ee1fa2f59
Chris M <webmaster@magnaws.com>
2018-03-23 22:51:45 -0400

    Documented the main DB class.

--- db/co_main_db_record.class.php
+++ db/co_main_db_record.class.php
@@ -53,11 +53,15 @@ class CO_Main_DB_Record extends A_CO_DB_Table_Base {
 
     /***********************/
     /**
+    This sets up the instance, based on a supplied associative array of database values and keys.
+    
+    \returns TRUE, if sucessful.
      */
-    protected function _load_from_db($in_db_result) {
-        $ret = parent::_load_from_db($in_db_result);
+    protected function _load_from_db(   $in_db_result   ///< This is the associative array of database values.
+                                    ) {
+        $ret = parent::_load_from_db($in_db_result);    // Start by calling the base class version.
         
-        if ($ret) {
+        if ($ret) { // If that went OK, we add our own two cents...
             $this->class_description = 'Base Class for Main Database Records.';
             $this->name = (isset($this->name) && trim($this->name)) ? trim($this->name) : "Base Class Instance ($this->_id)";
             
@@ -95,9 +99,12 @@ class CO_Main_DB_Record extends A_CO_DB_Table_Base {
     
     /***********************/
     /**
+    This adds our data to the saved parameter associative array.
+    
+    \returns an associative array of instance values, using the database keys.
      */
     protected function _build_parameter_array() {
-        $ret = parent::_build_parameter_array();
+        $ret = parent::_build_parameter_array();    // Start with the base class.
         
         $ret['owner'] = $this->owner_id;
         for ($tag_no = 0; $tag_no < 10; $tag_no++) {
@@ -112,8 +119,14 @@ class CO_Main_DB_Record extends A_CO_DB_Table_Base {
     
     /***********************/
     /**
+    This gets the payload for storage, encrypting it, if necessary.
+    
+    This is meant for saving the payload into the database.
+    
+    \returns the payload, in whatever form it takes.
      */
-    protected function _get_storage_payload() {
+    protected function _get_storage_payload(    $in_key = NULL  ///< This is an optional private key. If provided, the returned payload will be encrypted.
+                                            ) {
         $ret = $this->_get_encrypted_payload();
         
         if (!$ret) {
@@ -125,8 +138,26 @@ class CO_Main_DB_Record extends A_CO_DB_Table_Base {
     
     /***********************/
     /**
+    This is called to decrypt the payload with the provided private key.
      */
-    protected function _get_decrypted_payload(  $in_key = NULL
+    protected function _decrypt_payload_with_private_key(   $in_private_key ///< The private key to use for decryption.
+                                                    ) {
+        $ret = NULL;
+        
+        if (!openssl_private_decrypt($this->_raw_payload, $ret, $in_private_key)) {
+            $ret = NULL;
+        }
+        
+        return $ret;
+    }
+    
+    /***********************/
+    /**
+    This is called to decrypt the payload from the database.
+    
+    \returns the decrypted payload.
+     */
+    protected function _get_decrypted_payload(  $in_key ///< This is the private key to be used for decrypting. It should be the same one stored for the login.
                                             ) {
         $ret = NULL;
 
@@ -145,6 +176,26 @@ class CO_Main_DB_Record extends A_CO_DB_Table_Base {
     
     /***********************/
     /**
+    This is called to encrypt the payload (for storage in the DB). It uses a provided private key.
+    
+    \returns the encrypted payload.
+     */
+    protected function _encrypt_payload_with_private_key(   $in_private_key ///< The key to use for encryption.
+                                                    ) {
+        $ret = NULL;
+        
+        if (!openssl_private_encrypt($this->_raw_payload, $ret, $in_private_key)) {
+            $ret = NULL;
+        }
+        
+        return $ret;
+    }
+    
+    /***********************/
+    /**
+    This is called to encrypt the payload (for storage in the DB). It uses the stored private key.
+    
+    \returns the encrypted payload.
      */
     protected function _get_encrypted_payload() {
         $ret = NULL;
@@ -162,52 +213,28 @@ class CO_Main_DB_Record extends A_CO_DB_Table_Base {
         return $ret;
     }
     
-    /***********************/
-    /**
-     */
-    protected function _encrypt_payload_with_private_key(   $in_private_key
-                                                    ) {
-        $ret = NULL;
-        
-        if (!openssl_private_encrypt($this->_raw_payload, $ret, $in_private_key)) {
-            $ret = NULL;
-        }
-        
-        return $ret;
-    }
-    
-    /***********************/
-    /**
-     */
-    protected function _decrypt_payload_with_private_key(   $in_private_key
-                                                    ) {
-        $ret = NULL;
-        
-        if (!openssl_private_decrypt($this->_raw_payload, $ret, $in_private_key)) {
-            $ret = NULL;
-        }
-        
-        return $ret;
-    }
-    
     /***********************************************************************************************************************/
     /***********************/
     /**
+    The initializer.
      */
-	public function __construct(    $in_db_object = NULL,
-	                                $in_db_result = NULL,
-	                                $in_owner_id = NULL,
-	                                $in_tags = NULL
+	public function __construct(    $in_db_object = NULL,   ///< The database object for this instance.
+	                                $in_db_result = NULL,   ///< The database row for this instance (associative array, with database keys).
+	                                $in_owner_id = NULL,    ///< The ID of the object (in the database) that "owns" this instance.
+	                                $in_tags_array = NULL   ///< An array of strings, up to ten elements long, for the tags.      
                                 ) {
-        $this->owner_id = $in_owner_id;
-        $this->tags = $in_tags;
+        $this->owner_id = intval($in_owner_id);
+        $this->tags = array_map(function($in) { return strval($in); }, $in_tags_array);
         parent::__construct($in_db_object, $in_db_result);
     }
     
     /***********************/
     /**
+    Simple setter for the owner ID.
+    
+    \returns TRUE, if successful.
      */
-    public function set_owner_id($in_new_id
+    public function set_owner_id(   $in_new_id  ///< The new value
                                         ) {
         $ret = FALSE;
         
@@ -221,8 +248,11 @@ class CO_Main_DB_Record extends A_CO_DB_Table_Base {
     
     /***********************/
     /**
+    Simple setter for the tags.
+    
+    \returns TRUE, if successful.
      */
-    public function set_tags($in_tags_array
+    public function set_tags(   $in_tags_array  ///< An array of strings, up to ten elements long, for the tags.
                             ) {
         $ret = FALSE;
         
@@ -236,9 +266,12 @@ class CO_Main_DB_Record extends A_CO_DB_Table_Base {
     
     /***********************/
     /**
+    Setter for one tag, by index.
+    
+    \returns TRUE, if successful.
      */
-    public function set_tag($in_tag_index,
-                            $in_tag_value
+    public function set_tag(    $in_tag_index,  ///< The index (0-based -0 through 9) of the tag to set.
+                                $in_tag_value   ///< A string, with the tag value.
                             ) {
         $ret = FALSE;
         
@@ -248,8 +281,7 @@ class CO_Main_DB_Record extends A_CO_DB_Table_Base {
             if (!isset($this->tags) || !$this->tags) {
                 $this->tags = Array();
             }
-            $in_tag_value = strval($in_tag_value);
-            $this->tags[$in_tag_index] = $in_tag_value;
+            $this->tags[$in_tag_index] = strval($in_tag_value);
             
             $ret = $this->update_db();
         }
@@ -259,8 +291,11 @@ class CO_Main_DB_Record extends A_CO_DB_Table_Base {
     
     /***********************/
     /**
+    Returns the payload, decrypted, if requested.
+    
+    \returns the payload, in whatever form it takes.
      */
-    public function get_payload(    $in_key = NULL
+    public function get_payload(    $in_key = NULL  ///< This is an optional private key. If provided, the returned payload will be decrypted.
                                 ) {
         $ret = NULL;
         
@@ -275,9 +310,12 @@ class CO_Main_DB_Record extends A_CO_DB_Table_Base {
     
     /***********************/
     /**
+    Sets the payload, encrypting with the login key, if requested.
+    
+    \returns TRUE, if successful.
      */
-    public function set_payload(    $in_payload,
-                                    $encrypted = FALSE
+    public function set_payload(    $in_payload,        ///< The raw, unencrypted payload to be stored.
+                                    $encrypted = FALSE  ///< TRUE if we want the payload encrypted.
                                 ) {
         $ret = FALSE;
         
