d03cae2c2509f28bffadba0220697f417fd777bd
Chris M <webmaster@magnaws.com>
2018-05-12 16:28:32 -0400

    Did some work preparing the token label class. We'll need to finish the work in COBRA.

--- db_classes/co_token_labels.class.php
+++ db_classes/co_token_labels.class.php
@@ -24,6 +24,8 @@ require_once(CO_Config::db_class_dir().'/co_security_node.class.php');
 This class is used to assign string labels to security tokens.
 It is edited by authorized managers.
 It uses the instance context to store the labels.
+
+This is a weird class. Even though it lives in BADGER, it needs to be tested by COBRA. It will crash BADGER, if BADGER is not instantiated into COBRA.
  */
 class CO_Token_Labels extends CO_Security_Node {
     /***********************************************************************************************************************/
@@ -36,7 +38,7 @@ class CO_Token_Labels extends CO_Security_Node {
                                 ) {
         $this->context['token_strings'] = Array();
         parent::__construct($in_db_object, $in_db_result);
-        $this->class_description = 'This is an abstract base class for displaying strings associated with security tokens';
+        $this->class_description = 'This is a class for displaying strings associated with security tokens';
         $this->instance_description = isset($this->name) && $this->name ? "$this->name ($this->_id)" : "Unnamed Token String Node ($this->_id)";
     }
 
@@ -58,6 +60,50 @@ class CO_Token_Labels extends CO_Security_Node {
     
     /***********************/
     /**
+    This is a getter for the "owner" login manager ID. This allows us to "see" the ID, even if we aren't cleared for it, as it's used as a lookup.
+    
+    \returns the manager ID.
+     */
+    public function get_manager_id() {
+        return intval($this->context['manager_id']);
+    }
+    
+    /***********************/
+    /**
+    This is a security-vetted getter for the login manager object. It will return NULL if we can't see the manager login.
+    
+    \returns the CO_Login_Manager instance, or NULL.
+     */
+    public function get_manager_object() {
+        $m_id = $this->get_manager_id();
+        
+        if ($m_id) {
+            return $this->get_access_object()->get_login_item($m_id);
+        }
+        
+        return NULL;
+    }
+    
+    /***********************/
+    /**
+    This sets the ID of the "owner" manager. It's security-vetted.
+    
+    We can set a manager ID that we can't "see," but this operation should only be done by the "manager" that owns this, or another manager.
+    
+    \returns TRUE, if the set was successful.
+     */
+    public function set_manager_id( $in_manager_id  ///< This is the integer security DB ID of the manager login that "owns" this instance.
+                                    ) {
+        
+        if ($this->user_can_edit_ids()) {
+            $this->context['manager_id'] = intval($in_manager_id);
+            
+            $ret = $this->update_db();
+        }
+    }
+    
+    /***********************/
+    /**
     This is a security-vetted "getter" for a token identifier string.
     
     \returns a string, with the token identifier string. Empty string if the string does not exist, or the current user cannot see that token.
@@ -74,9 +120,9 @@ class CO_Token_Labels extends CO_Security_Node {
         $lang = $this->get_access_object()->get_lang();
         
         if (in_array($in_security_token_id, $my_ids)) { // We get nothing unless we can see the ID.
-            if (isset($this->context['token_strings']) {
-                if (isset($this->context['token_strings'][$in_security_token_id]) {
-                    if (isset($this->context['token_strings'][$in_security_token_id][$lang]) {
+            if (isset($this->context['token_strings'])) {
+                if (isset($this->context['token_strings'][$in_security_token_id])) {
+                    if (isset($this->context['token_strings'][$in_security_token_id][$lang])) {
                         $ret = $this->context['token_strings'][$in_security_token_id[$lang]];
                     }
                 }
@@ -105,7 +151,7 @@ class CO_Token_Labels extends CO_Security_Node {
             $my_ids = array_map('intval', $my_ids);
         
             if (in_array($in_security_token_id, $my_ids)) { // Next, the user has to have access to the token they want to modify.
-                if (!isset($this->context['token_strings']) {
+                if (!isset($this->context['token_strings'])) {
                     $this->context['token_strings'] = Array();
                 }
             
@@ -116,6 +162,26 @@ class CO_Token_Labels extends CO_Security_Node {
             }
         }
                 
+        return $ret;
+    }
+    
+    /***********************/
+    /**
+    Only managers can edit, and they need to have the write token for this instance.
+    
+    \returns TRUE, if the current logged-in user can edit IDs for this login.
+     */
+    public function user_can_edit_ids() {
+        $ret = parent::user_can_edit_ids;
+        
+        if (!$ret) {
+            $login_object = $this->get_access_object()->get_login_tem();
+            
+            if (isset($login_object) && ($login_object instanceof CO_Login_Manager)) {
+                $ret = $this->user_can_write();
+            }
+        }
+        
         return $ret;
     }
 };
